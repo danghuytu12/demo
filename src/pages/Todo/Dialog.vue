ae721ac6f3436d02a55785eae81689fe32da6cda
Huy Tú <danghuytu.thd2006@gmail.com>
2022-08-04 00:04:06 +0700

    first commit

--- src/pages/Todo/Dialog.vue
+++ src/pages/Todo/Dialog.vue
@@ -6,25 +6,12 @@
         <v-card-title>
           <span class="headline">{{ formTitle }}</span>
         </v-card-title>
-        <!-- <v-card-text>
-          <v-container grid-list-md>
-            <v-layout wrap>
-              <v-flex xs12 sm6 md12>
-                <v-text-field v-model="editedItem.name" label="Name" :error-messages="nameErrors" :counter="10" required
-                  @input="$v.name.$touch()" @blur="$v.name.$touch()"></v-text-field>
-              </v-flex>
-              <v-flex xs12 sm6 md12>
-                <v-text-field v-model="editedItem.age" label="Age" :error-messages="nameErrors" :counter="10" required
-                  @input="$v.name.$touch()" @blur="$v.name.$touch()"></v-text-field>
-              </v-flex>
 
-            </v-layout>
-          </v-container>
-        </v-card-text> -->
         <v-form ref="form" v-model="valid" lazy-validation>
           <v-text-field v-model="editedItem.name" :counter="10" :rules="nameRules" label="Name" required></v-text-field>
 
-          <v-text-field v-model="editedItem.age" :rules="numberRule" label="Age" required></v-text-field>
+          <v-text-field  v-model="editedItem.age" :rules="numberRule" label="Age" required></v-text-field>
+          
         </v-form>
 
         <v-card-actions>
@@ -58,7 +45,7 @@ export default {
       v => !!v || 'Name is required',
       v => (v && v.length <= 10) || 'Name must be less than 10 characters',
     ],
-    numberRule: [v => (!isNaN(parseFloat(v)) && v >= 0 && v <= 150) || 'Number has to be between 0 and 999']
+    numberRule: [v => (!isNaN(parseFloat(v)) && v >= 0 && v <= 150) || 'Number has to be between 0 and 150']
   }),
   methods: {
     save() {
